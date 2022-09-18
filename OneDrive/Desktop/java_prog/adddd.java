44d47202e95c7cb3cf8718207bf75e0253c99ebf
Aashish Kumar Yadav <aashishy292@gmail.com>
2022-09-18 15:32:31 +0530

    initial commit

---
+++ OneDrive/Desktop/java_prog/adddd.java
@@ -0,0 +1,10 @@
+public class adddd {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 7;
+        int c = 3;
+        int sum =a+b+c;
+        System.out.println("sum "+sum);
+    }
+    
+}
